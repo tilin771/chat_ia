1b20dc1e082a8fe948393d0af65915584523776e
Jon Kevin Goncalves Hurtado <jonkevin.goncalves@servosa.com>
2025-10-06 11:50:32 +0200

    Singleton session implemented

--- services/query_kb.py
+++ services/query_kb.py
@@ -1,22 +1,18 @@
-from data.connections import Session, MODEL_ARN, KB_ID
+from data.connections import bedrock_agent_client, MODEL_ARN, KB_ID
 
 
 def consultar_kb_streaming(input_text, prioridad=7):
-    session = Session(profile_name="699541216231_IA_Dev", region_name="us-east-1")
-    bedrock_agent_client = session.client("bedrock-agent-runtime")
-    
     metadata_filter = {"lessThanOrEquals": {"key": "Prioridad", "value": prioridad}}
-    
+
     # Añadimos instrucción global
     prompt = (
-    f"Analiza la siguiente consulta: {input_text}\n\n"
-    "Estilo: Responde detalladamente yendo al grano a la solución del problema"
-    " Instrucciones obligatorias:\n"
-    "- Si encuentras información relevante en la base de conocimiento, respóndela normalmente siguiendo el estilo indicado.\n"
-    "- Si NO hay información relevante, responde *únicamente* con la palabra exacta: create_ticket\n"
-    "- No agregues explicaciones, frases adicionales, saludos, ni justificaciones si respondes create_ticket.\n"
-)
-
+        f"Analiza la siguiente consulta: {input_text}\n\n"
+        "Estilo: Responde detalladamente yendo al grano a la solución del problema"
+        " Instrucciones obligatorias:\n"
+        "- Si encuentras información relevante en la base de conocimiento, respóndela normalmente siguiendo el estilo indicado.\n"
+        "- Si NO hay información relevante, responde *únicamente* con la palabra exacta: create_ticket\n"
+        "- No agregues explicaciones, frases adicionales, saludos, ni justificaciones si respondes create_ticket.\n"
+    )
 
     response = bedrock_agent_client.retrieve_and_generate_stream(
         input={"text": prompt},
@@ -38,4 +34,3 @@ def consultar_kb_streaming(input_text, prioridad=7):
     for event in response['stream']:
         if 'output' in event and 'text' in event['output']:
             yield event['output']['text']
-
